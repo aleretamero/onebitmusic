024ec2ab8572cafffbe1c1ac41368fcb81b10e7b
aleretamero <alexandreretamero@gmail.com>
2023-07-03 21:03:33 -0300

    estrutura header

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,7 +1,35 @@
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
+
+import Image from 'next/image';
+
+import onebitmusic from '../../../../public/onebitcode.svg';
+import logo from '../../../../public/logo.svg';
+import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Image src={onebitmusic} alt="onebitmusic" />
+        <Image src={logo} alt="logo" />
+      </div>
+      <nav className={styles.nav}>
+        <Link href="" className={styles.link}>
+          INICIO
+        </Link>
+        <Link href="" className={styles.link}>
+          ARTISTAS
+        </Link>
+        <Link href="" className={styles.link}>
+          PROGRAMAÇÃO
+        </Link>
+        <Link href="" className={styles.link}>
+          CONTATO
+        </Link>
+        <Link href="" className="button">
+          COMPRAR INGRESSOS
+        </Link>
+      </nav>
+    </header>
+  );
+};
