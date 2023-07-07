2f4906599c1b01b984455d2a73a2d809e9940ba9
aleretamero <alexandreretamero@gmail.com>
2023-07-07 17:29:25 -0300

    change meta tags

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -1,45 +1,24 @@
+const url = 'https://onebitmusic-zeta.vercel.app/';
+const title = 'Onebitmusic - Especialistas em eventos músicais';
+const description =
+  'Na onebitmusic, estamos redefinindo a maneira como experimentamos e nos envolvemos com a música, utilizando tecnologia de realidade virtual (VR) para criar shows e performances que transcendem os limites físicos e levam você a novos patamares de entretenimento musical.';
+
 export const Meta = () => {
   return (
     <>
       {/* Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://metatags.io" />
-      <meta
-        property="og:title"
-        content="Onebitmusic - Especialistas em eventos músicais"
-      />
-      <meta
-        property="og:description"
-        content=" Na onebitmusic, estamos redefinindo a maneira como experimentamos e
-      nos envolvemos com a música, utilizando tecnologia de realidade
-      virtual (VR) para criar shows e performances que transcendem os
-      limites físicos e levam você a novos patamares de entretenimento
-      musical."
-      />
-      <meta
-        property="og:image"
-        content="https://onebitmusic-ten.vercel.app/images/hero.webp"
-      />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={`${url}/images/hero.webp`} />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://metatags.io" />
-      <meta
-        property="twitter:title"
-        content="Onebitmusic - Especialistas em eventos músicais"
-      />
-      <meta
-        property="twitter:description"
-        content=" Na onebitmusic, estamos redefinindo a maneira como experimentamos e
-      nos envolvemos com a música, utilizando tecnologia de realidade
-      virtual (VR) para criar shows e performances que transcendem os
-      limites físicos e levam você a novos patamares de entretenimento
-      musical."
-      />
-      <meta
-        property="twitter:image"
-        content="https://onebitmusic-ten.vercel.app/images/hero.webp"
-      />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={`${url}/images/hero.webp`} />
     </>
   );
 };
