f694d136a2557edb771228adbb3b4d89df0b3dd3
aleretamero <alexandreretamero@gmail.com>
2023-07-07 19:40:57 -0300

    update

--- src/components/EventSchedule/eventsData.ts
+++ src/components/EventSchedule/eventsData.ts
@@ -79,12 +79,12 @@ export const events: TypeEvents = {
         "Venha se divertir com a banda 'Null and the Undefined', conhecida por seus hits animados e letras divertidas. Prepare-se para dançar e cantar junto com eles!",
       artists: [
         {
-          image: '/images/artists/Avatar-4.svg',
+          image: '/images/artists/Avatar-6.png',
           name: 'Null',
           booth: '3F15',
         },
         {
-          image: '/images/artists/Avatar-4.svg',
+          image: '',
           name: 'Undefined',
           booth: '3F15',
         },
@@ -97,12 +97,12 @@ export const events: TypeEvents = {
         '"Spaceship Destroyer e 404 Not Found" - A dupla mais explosiva da galáxia musical! Spaceship Destroyer, o guitarrista intergaláctico com solos destruidores, se une a 404 Not Found, o baterista perdido em seu próprio ritmo cósmico.',
       artists: [
         {
-          image: '/images/artists/Avatar-4.svg',
+          image: '/images/artists/Avatar-7.png',
           name: '404 Not Found',
           booth: '4F18',
         },
         {
-          image: '/images/artists/Avatar-4.svg',
+          image: '/images/artists/Avatar-8.jpeg',
           name: 'Spaceship Destroyer',
           booth: '9FEC',
         },
@@ -115,12 +115,12 @@ export const events: TypeEvents = {
         '"Ziri Piri e Infinite Loop" - A dupla mais improvável da música algoritmicamente estranha! Ziri Piri, o mestre das letras sem sentido, une forças com Infinite Loop, o produtor de batidas frenéticas e loops intermináveis.',
       artists: [
         {
-          image: '/images/artists/Avatar-4.svg',
+          image: '/images/artists/Avatar-9.jpeg',
           name: 'Infinite Loop',
           booth: '5F21',
         },
         {
-          image: '/images/artists/Avatar-4.svg',
+          image: '/images/artists/Avatar-10.jpg',
           name: 'Ziri Piri',
           booth: '8C95',
         },
@@ -132,12 +132,12 @@ export const events: TypeEvents = {
     {
       time: '1:00 PM to 2:00 PM',
       description:
-        "Venha se divertir com a banda 'Error 404' e sua música peculiarmente engraçada. Com letras que vão fazer você rir e melodias cativantes, prepare-se para uma tarde de diversão e boas risadas.",
+        'Prepare-se para uma experiência musical futurística com a banda "Cyberwave Coders". Utilizando sintetizadores e sons inspirados no universo da programação, eles criarão paisagens sonoras imersivas e melodias eletrônicas cativantes.',
       artists: [
         {
-          image: '/images/artists/Avatar-4.svg',
-          name: 'Error 404',
-          booth: '6D23',
+          image: '/images/artists/Avatar-11.jpeg',
+          name: 'Cyberwave Coders',
+          booth: '6F09',
         },
       ],
       status: 'coming-soon',
@@ -148,12 +148,12 @@ export const events: TypeEvents = {
         '"Syntax Error" - A banda que vai desafiar seus ouvidos e seu senso de realidade com suas composições musicais totalmente fora do padrão. Prepare-se para uma experiência sonora caótica e intrigante.',
       artists: [
         {
-          image: '/images/artists/Avatar-4.svg',
+          image: '/images/artists/Avatar-12.jpeg',
           name: 'Syntax',
           booth: '7E10',
         },
         {
-          image: '/images/artists/Avatar-4.svg',
+          image: '/images/artists/Avatar-13.jpeg',
           name: 'Error',
           booth: '7E10',
         },
@@ -163,12 +163,12 @@ export const events: TypeEvents = {
     {
       time: '5:00 PM to 6:00 PM',
       description:
-        'Prepare-se para um encontro musical de outro mundo com a dupla "Byte Brothers". Esses músicos virtuosos vão levar você a uma viagem sonora através do universo digital, combinando sons eletrônicos e melodias viciantes.',
+        'Apresentando a banda "The Bug Hunters" e sua abordagem inovadora da música eletrônica. Eles combinam batidas pulsantes com sons de bugs de programação e samples de código para criar uma experiência sonora única e envolvente.',
       artists: [
         {
-          image: '/images/artists/Avatar-4.svg',
-          name: 'Byte Brothers',
-          booth: '2B37',
+          image: '/images/artists/Avatar-14.png',
+          name: 'The Bug Hunters',
+          booth: '4D21',
         },
       ],
       status: 'coming-soon',
--- src/components/Testimonials/reportsData.ts
+++ src/components/Testimonials/reportsData.ts
@@ -18,7 +18,7 @@ export const reportsData: TypeReport[] = [
       'Através dos shows digitais e da tecnologia de realidade virtual da OneBitMusic, pude mergulhar em um universo musical fascinante. Os visuais deslumbrantes, a qualidade do áudio e a interatividade oferecida pela plataforma me deixaram maravilhado. A OneBitMusic realmente entende como proporcionar experiências musicais únicas e memoráveis.',
     profileName: 'Carlos',
     profileAvatar: '/images/avatar/avatar-2.svg',
-    ProfileCity: 'Lisboa',
+    ProfileCity: 'Rio de Janeiro',
   },
   {
     report:
@@ -26,5 +26,26 @@ export const reportsData: TypeReport[] = [
     profileName: 'Ana',
     profileAvatar: '/images/avatar/avatar-3.svg',
     ProfileCity: 'São Paulo',
+  },
+  {
+    report:
+      'Eu sempre gostei de música, mas nunca tive a oportunidade de ir a muitos shows ao vivo. Com a OneBitMusic, isso mudou. A plataforma me permite assistir a shows digitais de artistas incríveis, sem sair de casa. A realidade virtual me faz sentir parte da performance, e eu posso interagir com outros fãs e até mesmo com os próprios artistas. A OneBitMusic é uma forma inovadora e divertida de apreciar a música.',
+    profileName: 'Ling',
+    profileAvatar: '/images/avatar/avatar-4.jpg',
+    ProfileCity: 'Pequim',
+  },
+  {
+    report:
+      'Eu sou um grande fã de música independente e sempre procuro novas formas de descobrir e apoiar artistas emergentes. A OneBitMusic é uma plataforma perfeita para isso. Ela me oferece uma variedade de shows digitais de diferentes gêneros e estilos, e me permite conhecer os artistas por trás da música. A realidade virtual me proporciona uma experiência imersiva e envolvente, que me faz sentir mais próximo dos artistas e da sua arte.',
+    profileName: 'Ryan',
+    profileAvatar: '/images/avatar/avatar-5.jpg',
+    ProfileCity: 'Toronto',
+  },
+  {
+    report:
+      'Eu adoro a OneBitMusic porque ela me permite explorar novos mundos musicais. A plataforma me apresenta a artistas de diferentes países e culturas, e me faz viajar através da música. A realidade virtual me transporta para cenários incríveis, onde eu posso ver e ouvir os artistas em ação. A OneBitMusic é uma forma de expandir os meus horizontes musicais e culturais.',
+    profileName: 'Nisa',
+    profileAvatar: '/images/avatar/avatar-6.jpg',
+    ProfileCity: 'Bangkok',
   },
 ];
