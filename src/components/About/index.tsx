cd399cab6667adb92a91cd1288f08af0c2aabab2
aleretamero <alexandreretamero@gmail.com>
2023-07-08 16:44:45 -0300

    update

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -14,29 +14,36 @@ export const About = () => {
   const [imageBlur, setImageBlur] = useState('image2');
 
   return (
-    <section
-      className={`${styles.container} limitedSection`}
-      data-aos="fade-down-right"
-    >
-      <div className={styles.containerImages}>
-        <Image
-          src={imageAbout1}
-          alt="ImageShows"
-          width={480}
-          height={480}
-          quality={100}
+    <section className={`${styles.container} limitedSection`}>
+      <div className={styles.containerImages} data-aos="zoom-in">
+        <div
+          className={`${styles.contentImage1} ${
+            imageBlur === 'image1' && styles.imageBlur
+          }`}
           onClick={() => setImageBlur('image2')}
-          style={imageBlur === 'image1' ? { zIndex: 0, opacity: '30%' } : {}}
-        />
-        <Image
-          src={imageAbout2}
-          alt="ImageShows"
-          width={480}
-          height={480}
-          quality={100}
+        >
+          <Image
+            src={imageAbout1}
+            alt="ImageShows"
+            width={460}
+            height={480}
+            quality={100}
+          />
+        </div>
+        <div
+          className={`${styles.contentImage2} ${
+            imageBlur === 'image2' && styles.imageBlur
+          }`}
           onClick={() => setImageBlur('image1')}
-          style={imageBlur === 'image2' ? { zIndex: 0, opacity: '30%' } : {}}
-        />
+        >
+          <Image
+            src={imageAbout2}
+            alt="ImageShows"
+            width={460}
+            height={480}
+            quality={100}
+          />
+        </div>
       </div>
 
       <div className={styles.content} data-aos="fade-down-left">
