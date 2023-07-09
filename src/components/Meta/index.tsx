df3d7773e5f01bb5c3a26f554eab155f22b3a4e7
aleretamero <alexandreretamero@gmail.com>
2023-07-09 13:17:29 -0300

    include png onebitmusic

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -11,14 +11,14 @@ export const Meta = () => {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`${url}/onebitcode.svg`} />
+      <meta property="og:image" content={`${url}/onebitcode.png`} />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={`${url}/onebitcode.svg`} />
+      <meta property="twitter:image" content={`${url}/onebitcode.png`} />
     </>
   );
 };
